fecfbdbbdfedf9353e555f42d8e8ec727bffcbbf
pdro-h0 <pedro.pho792@gmail.com>
2024-01-31 15:54:35 -0300

    feat: add initial booking component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,6 +3,7 @@ import { format } from "date-fns";
 import Header from "@/components/header";
 import { ptBR } from "date-fns/locale/pt-BR";
 import Search from "./components/search";
+import BookingItem from "@/components/booking-item";
 
 export default function Home() {
   return (
@@ -20,6 +21,13 @@ export default function Home() {
 
       <div className="px-5 mt-6">
         <Search />
+      </div>
+
+      <div className="px-5 mt-6">
+        <h2 className="text-sm uppercase text-zinc-400 font-bold mb-3">
+          Agendamentos
+        </h2>
+        <BookingItem />
       </div>
     </div>
   );
---
+++ components/booking-item.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Card, CardContent } from "./ui/card";
+import { Badge } from "./ui/badge";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+const BookingItem = () => {
+  return (
+    <Card>
+      <CardContent className="p-5 flex justify-between py-0">
+        <div className="flex flex-col gap-2 py-5">
+          <Badge className="bg-[#221c30] text-primary hover:bg-[#221c30] w-fit">
+            CONFIRMADO
+          </Badge>
+          <h2 className="font-bold">Corte de Cabelo</h2>
+
+          <div className="flex items-center gap-2">
+            <Avatar className="h-6 w-6">
+              <AvatarImage src="https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png" />
+              <AvatarFallback>AA</AvatarFallback>
+            </Avatar>
+
+            <h3 className="text-sm">Barbearia Tal</h3>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center justify-center px-3 border-l border-solid border-secondary">
+          <p className="text-sm">Maio</p>
+          <p className="text-2xl font-bold">06</p>
+          <p className="text-sm">09:45</p>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default BookingItem;
