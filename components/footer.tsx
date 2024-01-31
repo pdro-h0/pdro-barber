7bf6cdad328caf32c3f942a36bdb33762a3fbcc8
pdro-h0 <pedro.pho792@gmail.com>
2024-01-31 20:20:45 -0300

    feat: add footer component

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="w-full bg-secondary py-6 px-5">
+      <p className="text-zinc-400 text-xs font-bold opacity-75">
+        © 2024 Copyright PDR Barber
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
