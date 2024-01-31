e5943e543c34c1bf260b47398dd77fda51554757
pdro-h0 <pedro.pho792@gmail.com>
2024-01-31 11:03:05 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,21 @@
+import { format } from "date-fns";
+
 import Header from "@/components/header";
+import { ptBR } from "date-fns/locale/pt-BR";
 
 export default function Home() {
   return (
-    <div><Header /></div>
+    <div>
+      <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, [NOME DO USUÁRIO]!</h2>
+        <p className="capitalize">
+          {format(new Date(), "EEEE ',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
+    </div>
   );
 }
