8c4bc4e15fddfda35aeb21ea689da3d8ce4e0dbd
pdro-h0 <pedro.pho792@gmail.com>
2024-02-02 10:15:11 -0300

    feat: add SideMenu component

--- components/header.tsx
+++ components/header.tsx
@@ -1,38 +1,19 @@
 "use client";
 
 import React from "react";
+
 import { Card, CardContent } from "./ui/card";
+
 import Link from "next/link";
+
 import { Button } from "./ui/button";
-import {
-  CalendarIcon,
-  HomeIcon,
-  LogInIcon,
-  LogOutIcon,
-  MenuIcon,
-  UserCircleIcon,
-} from "lucide-react";
-import { signIn, signOut, useSession } from "next-auth/react";
-import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "./ui/sheet";
-import { Avatar, AvatarImage } from "./ui/avatar";
+
+import { MenuIcon } from "lucide-react";
+
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import SideMenu from "./side-menu";
 
 const Header = () => {
-  const { data } = useSession();
-
-  const handleLogin = async () => {
-    await signIn("google");
-  };
-
-  const handleLogout = async () => {
-    await signOut();
-  };
-
   return (
     <Card>
       <CardContent className="p-5 flex justify-between items-center">
@@ -45,61 +26,8 @@ const Header = () => {
             </Button>
           </SheetTrigger>
 
-          <SheetContent className="justify-between text-left border-b border-slid border-secondary p-5">
-            <SheetHeader>
-              <SheetTitle>Menu</SheetTitle>
-            </SheetHeader>
-
-            {data?.user ? (
-              <div className="flex justify-between px-5 py-6 items-center">
-                <div className="flex items-center gap-3">
-                  <Avatar>
-                    <AvatarImage src={data.user?.image ?? ""} />
-                  </Avatar>
-
-                  <h2 className="font-bold">{data.user.name}</h2>
-                </div>
-
-                <Button variant="secondary" size="icon" onClick={handleLogout}>
-                  <LogOutIcon size={18}/>
-                </Button>
-              </div>
-            ) : (
-              <div className="flex flex-col px-5 py-6 gap-3">
-                <div className="flex items-center gap-2">
-                  <UserCircleIcon size={32} />
-
-                  <h2 className="font-bold">Olá, faça seu login!</h2>
-                </div>
-
-                <Button
-                  variant="secondary"
-                  className="w-full"
-                  onClick={handleLogin}
-                >
-                  <LogInIcon className="mr-2" size={18} />
-                  Fazer Login
-                </Button>
-              </div>
-            )}
-
-            <div className="flex flex-col gap-3 px-5">
-              <Button variant="outline" className="justify-start" asChild>
-                <Link href="/">
-                  <HomeIcon size={18} className="mr-2" />
-                  Início
-                </Link>
-              </Button>
-
-              {data?.user && (
-                <Button variant="outline" className="justify-start" asChild>
-                  <Link href="/bookings">
-                    <CalendarIcon size={18} className="mr-2" />
-                    Agendamentos
-                  </Link>
-                </Button>
-              )}
-            </div>
+          <SheetContent className="p-0">
+            <SideMenu />
           </SheetContent>
         </Sheet>
       </CardContent>
---
+++ components/side-menu.tsx
@@ -0,0 +1,91 @@
+import Link from "next/link";
+
+import { Button } from "./ui/button";
+
+import {
+  CalendarIcon,
+  HomeIcon,
+  LogInIcon,
+  LogOutIcon,
+  UserCircleIcon,
+} from "lucide-react"; 
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
+import {
+  SheetHeader,
+  SheetTitle,
+} from "./ui/sheet";
+
+import { Avatar, AvatarImage } from "./ui/avatar";
+
+import React from "react";
+
+const SideMenu = () => {
+  const { data } = useSession();
+
+  const handleLogin = async () => {
+    await signIn("google");
+  };
+
+  const handleLogout = async () => {
+    await signOut();
+  };
+
+  return (
+    <>
+      <SheetHeader className="justify-between text-left border-b border-slid border-secondary p-5">
+        <SheetTitle>Menu</SheetTitle>
+      </SheetHeader>
+
+      {data?.user ? (
+        <div className="flex justify-between px-5 py-6 items-center">
+          <div className="flex items-center gap-3">
+            <Avatar>
+              <AvatarImage src={data.user?.image ?? ""} />
+            </Avatar>
+
+            <h2 className="font-bold">{data.user.name}</h2>
+          </div>
+
+          <Button variant="secondary" size="icon" onClick={handleLogout}>
+            <LogOutIcon size={18} />
+          </Button>
+        </div>
+      ) : (
+        <div className="flex flex-col px-5 py-6 gap-3">
+          <div className="flex items-center gap-2">
+            <UserCircleIcon size={32} />
+
+            <h2 className="font-bold">Olá, faça seu login!</h2>
+          </div>
+
+          <Button variant="secondary" className="w-full" onClick={handleLogin}>
+            <LogInIcon className="mr-2" size={18} />
+            Fazer Login
+          </Button>
+        </div>
+      )}
+
+      <div className="flex flex-col gap-3 px-5">
+        <Button variant="outline" className="justify-start" asChild>
+          <Link href="/">
+            <HomeIcon size={18} className="mr-2" />
+            Início
+          </Link>
+        </Button>
+
+        {data?.user && (
+          <Button variant="outline" className="justify-start" asChild>
+            <Link href="/bookings">
+              <CalendarIcon size={18} className="mr-2" />
+              Agendamentos
+            </Link>
+          </Button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default SideMenu;
